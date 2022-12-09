258a49a462a6866552e42e41dc2d1a1a6ddf180e
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2022-12-10 01:12:57 +0900

    正規表現を用いホワイトスペースを含む出力に対応

--- content.js
+++ content.js
@@ -118,7 +118,7 @@ class TaskTest extends TestCase
     public function testSampleIO(string $input, string $expected): void
     {
         $stringIo = fopen("data://text/plain,$input", 'r');
-        $this->expectOutputString($expected);
+        $this->expectOutputRegex("/^" . $expected . "(\\s+)?$/");
         solver($stringIo);
     }
 
