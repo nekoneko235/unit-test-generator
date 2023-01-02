1127f8cbbc408817d19dbede4b3b3110e8222a9a
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2023-01-02 21:31:07 +0900

    コメントを追加

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+/*
+ * プルダウンメニューで選択された言語を保存／表示されるようにする
+ *（現時点では必要のない機能だが、拡張性を持たせておく）
+ */
 (() => {
     chrome.storage.sync.get(['language'], (result) => {
         const options = document.getElementById('language').options;
